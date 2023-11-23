e151491cc91043579e9b36714aa50572c4411954
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2023-11-23 15:20:45 -0300

    Update 4_Contact.py

--- pages/4_Contact.py
+++ pages/4_Contact.py
@@ -53,33 +53,41 @@ def centrar_texto(texto, tamanho, color):
 st.write("#")
 
 with st.container():    
-    col50, col51, col52, col53, col54, col55, col56, col57, col58 = st.columns(9)
+    col50, col51, col52, col53, col54 = st.columns(5)
     with col51:
         centrar_imagen("https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/Kaggle_logo.png/200px-Kaggle_logo.png", 50)
     with col52:
         centrar_imagen("https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/GitHub_Invertocat_Logo.svg/200px-GitHub_Invertocat_Logo.svg.png", 50)
     with col53:
         centrar_imagen("https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/150px-Instagram_logo_2022.svg.png", 50)
-    with col54:
-        centrar_imagen("https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/2023_Facebook_icon.svg/50px-2023_Facebook_icon.svg.png", 50)
-    with col55:
-        centrar_imagen("https://upload.wikimedia.org/wikipedia/commons/thumb/a/aa/LinkedIn_2021.svg/200px-LinkedIn_2021.svg.png", 50)        
-    with col56:
-        centrar_imagen("https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/120px-WhatsApp.svg.png", 50)
-        
+            
 with st.container():    
-    col60, col61, col62, col63, col64, col65, col66, col67, col68 = st.columns(9)
+    col60, col61, col62, col63, col64 = st.columns(5)
     with col61:
         centrar_texto("[Kaggle](https://www.kaggle.com/willycerato)", 7, "blue")
     with col62:
         centrar_texto("[Github](https://github.com/Willy71)", 7, "blue")
     with col63:
         centrar_texto("[Instagram](https://www.instagram.com/willycerato)", 7, "blue")
-    with col64:
-        centrar_texto("[Facebook](https://www.facebook.com/guillermo.cerato)", 7, "blue")
-    with col65:
-        centrar_texto("[Linkedin](https://www.linkedin.com/in/willycerato/)", 7, "blue")
+    
+st.title("")
+st.title("")
+
+with st.container():    
+    col55, col56, col57, col58, col59 = st.columns(5)
+    with col56:
+        centrar_imagen("https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/2023_Facebook_icon.svg/50px-2023_Facebook_icon.svg.png", 50)
+    with col57:
+        centrar_imagen("https://upload.wikimedia.org/wikipedia/commons/thumb/a/aa/LinkedIn_2021.svg/200px-LinkedIn_2021.svg.png", 50)        
+    with col58:
+        centrar_imagen("https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/120px-WhatsApp.svg.png", 50)
+
+with st.container():    
+    col65, col66, col67, col68, col69 = st.columns(5)
     with col66:
+        centrar_texto("[Facebook](https://www.facebook.com/guillermo.cerato)", 7, "blue")
+    with col67:
+        centrar_texto("[Linkedin](https://www.linkedin.com/in/willycerato/)", 7, "blue")
+    with col68:
         centrar_texto("[Whatsapp](https://wa.me/5542991657847)", 7, "blue")
 
-st.title("")
