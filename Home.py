d9237f200e289218f379375cd7452126958d2a72
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2023-11-23 12:12:21 -0300

    Create Home.py

---
+++ Home.py
@@ -0,0 +1,55 @@
+import streamlit as st
+import pandas as pd
+import webbrowser
+from datetime import datetime
+
+# Colocar nome na pagina, icone e ampliar a tela
+st.set_page_config(
+    page_title="Futbol Argentino",
+    page_icon="⚽",
+    layout="wide"
+)
+
+df_data = pd.read_csv("datasets/futbol_argentino_logos.csv", index_col=0)
+
+page_bg_img = f"""
+<style>
+[data-testid="stAppViewContainer"] > .main {{
+background-image: url("https://i.postimg.cc/430MMcSq/wepik-export-20231112174143b-KKB.jpg");
+background-size: 180%;
+background-position: top left;
+background-repeat: repeat;
+background-attachment: local;
+}}
+
+[data-testid="stHeader"] {{
+background: rgba(0,0,0,0);
+}}
+
+[data-testid="stToolbar"] {{
+right: 2rem;
+}}
+
+[data-testid="stSidebar"] {{
+background: rgba(28,28,56,1);
+}}
+</style>
+"""
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
+
+def centrar_imagen(imagen, ancho):
+    # Aplicar estilo CSS para centrar la imagen con Markdown
+    st.markdown(
+        f'<div style="display: flex; justify-content: center;">'
+        f'<img src="{imagen}" width="{ancho}">'
+        f'</div>',
+        unsafe_allow_html=True
+    )
+
+
+def centrar_texto(texto, tamanho, color):
+    st.markdown(f"<h{tamanho} style='text-align: center; color: {color}'>{texto}</h{tamanho}>",
+                unsafe_allow_html=True)
+
+st.write("#")
