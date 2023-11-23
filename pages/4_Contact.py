db618fcf91f5c51edefdc993d15ebd3b01d54eb8
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2023-11-23 16:02:11 -0300

    Update 4_Contact.py

--- pages/4_Contact.py
+++ pages/4_Contact.py
@@ -49,7 +49,7 @@ def centrar_texto(texto, tamanho, color):
     st.markdown(f"<h{tamanho} style='text-align: center; color: {color}'>{texto}</h{tamanho}>",
                 unsafe_allow_html=True)
 
-def centrar_link(nombre, imagen, link):
+def photo_link(nombre, imagen, link):
     st.markdown(f"[![{nombre}]({imagen})]({link})")
 
 st.write("#")
@@ -57,42 +57,20 @@ st.write("#")
 with st.container():    
     col50, col51, col52, col53, col54 = st.columns(5)
     with col51:
-        centrar_imagen("https://e7.pngegg.com/pngimages/399/47/png-clipart-kaggle-predictive-modelling-data-science-business-predictive-analytics-%E6%95%B0%E6%8D%AE-blue-text.png", 80)
+        photo_link("Kaggle", "https://e7.pngegg.com/pngimages/399/47/png-clipart-kaggle-predictive-modelling-data-science-business-predictive-analytics-%E6%95%B0%E6%8D%AE-blue-text.png", "https://www.kaggle.com/willycerato")
     with col52:
-        centrar_imagen("https://static.vecteezy.com/system/resources/previews/016/833/880/non_2x/github-logo-git-hub-icon-with-text-on-white-background-free-vector.jpg", 80)
+        photo_link("Github", "https://static.vecteezy.com/system/resources/previews/016/833/880/non_2x/github-logo-git-hub-icon-with-text-on-white-background-free-vector.jpg", "https://github.com/Willy71")
     with col53:
-        centrar_imagen("https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/150px-Instagram_logo_2022.svg.png", 80)
+        photo_link("Instagram", "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/150px-Instagram_logo_2022.svg.png", "https://www.instagram.com/willycerato")
             
-with st.container():    
-    col60, col61, col62, col63, col64 = st.columns(5)
-    with col61:
-        centrar_texto("[Kaggle](https://www.kaggle.com/willycerato)", 7, "blue")
-    with col62:
-        centrar_texto("[Github](https://github.com/Willy71)", 7, "blue")
-    with col63:
-        centrar_texto("[Instagram](https://www.instagram.com/willycerato)", 7, "blue")
-    
 st.title("")
 st.title("")
 
 with st.container():    
     col55, col56, col57, col58, col59 = st.columns(5)
     with col56:
-        centrar_imagen("https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/2023_Facebook_icon.svg/50px-2023_Facebook_icon.svg.png", 80)
+        photo_link("Facebook", "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/2023_Facebook_icon.svg/50px-2023_Facebook_icon.svg.png", "https://www.facebook.com/guillermo.cerato")
     with col57:
-        centrar_imagen("https://img.freepik.com/vetores-premium/logotipo-quadrado-do-linkedin-isolado-no-fundo-branco_469489-892.jpg", 80)        
+        photo_link("Linkedin", "https://img.freepik.com/vetores-premium/logotipo-quadrado-do-linkedin-isolado-no-fundo-branco_469489-892.jpg", "https://www.linkedin.com/in/willycerato")        
     with col58:
-        centrar_imagen("https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/120px-WhatsApp.svg.png", 80)
-
-with st.container():    
-    col65, col66, col67, col68, col69 = st.columns(5)
-    with col66:
-        centrar_texto("[Facebook](https://www.facebook.com/guillermo.cerato)", 7, "blue")
-    with col67:
-        centrar_texto("[Linkedin](https://www.linkedin.com/in/willycerato/)", 7, "blue")
-    with col68:
-        centrar_texto("[Whatsapp](https://wa.me/5542991657847)", 7, "blue")
-
-centrar_link("Whatsapp", "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/120px-WhatsApp.svg.png", "https://wa.me/5542991657847")
-
-#st.markdown("[![Whatsapp](https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/120px-WhatsApp.svg.png)](https://wa.me/5542991657847)")
+        photo_link("Whatsapp", "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/120px-WhatsApp.svg.png", "Whatsapp](https://wa.me/5542991657847")
