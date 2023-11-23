01551310c4884cdff2b181e73b7d9e36be96d465
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2023-11-23 12:55:53 -0300

    Update Reservations.py

--- pages/Reservations.py
+++ pages/Reservations.py
@@ -1,1 +1,48 @@
+# Colocar nome na pagina, icone e ampliar a tela
+st.set_page_config(
+    page_title="Hotels Service",
+    page_icon=":house",
+    layout="wide"
+)
 
+page_bg_img = f"""
+<style>
+[data-testid="stAppViewContainer"] > .main {{
+background-image: url("https://i.postimg.cc/jdtSsJ9t/jr-korpa-H-BJWTh-ZRok-unsplash.jpg");
+background-size: 180%;
+background-position: top left;
+background-repeat: repeat;
+background-attachment: local;
+}}
+
+[data-testid="stHeader"] {{
+background: rgba(0,0,0,0);
+}}
+
+[data-testid="stToolbar"] {{
+right: 2rem;
+}}
+
+[data-testid="stSidebar"] {{
+background: rgba(28,28,56,1);
+}}
+</style>
+"""
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
+
+def centrar_imagen(imagen, ancho):
+    # Aplicar estilo CSS para centrar la imagen con Markdown
+    st.markdown(
+        f'<div style="display: flex; justify-content: center;">'
+        f'<img src="{imagen}" width="{ancho}">'
+        f'</div>',
+        unsafe_allow_html=True
+    )
+
+
+def centrar_texto(texto, tamanho, color):
+    st.markdown(f"<h{tamanho} style='text-align: center; color: {color}'>{texto}</h{tamanho}>",
+                unsafe_allow_html=True)
+
+st.write("#")
