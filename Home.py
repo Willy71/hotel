4d8df55617932db10d7b10ede375d04f1f4fd48a
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2023-11-23 12:54:06 -0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -13,7 +13,7 @@ st.set_page_config(
 page_bg_img = f"""
 <style>
 [data-testid="stAppViewContainer"] > .main {{
-background-image: url("https://i.postimg.cc/ZqCqGtQN/tim-mossholder-v-GNXOd-pg98-unsplash.jpg");
+background-image: url("https://i.postimg.cc/jdtSsJ9t/jr-korpa-H-BJWTh-ZRok-unsplash.jpg");
 background-size: 180%;
 background-position: top left;
 background-repeat: repeat;
