40939a789e9cebb78ca30e694498954d8ec26660
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2023-11-23 20:53:47 -0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -67,8 +67,8 @@ st.markdown("")
 centrar_texto("Application for temporary rental management.", 5, "white")
 centrar_texto("If you want customization of this project, consult with our developer.", 5, "white")
 
-col1, col2, col3, col4, col5, col6, col7 = st.columns(7)  # Establecer anchos personalizados (en este caso, la columna central es el doble de ancha)
-with col5:
+col1, col2, col3 = st.columns([5, 3.8, 2])  # Establecer anchos personalizados (en este caso, la columna central es el doble de ancha)
+with col2:
     photo_link("Whatsapp", "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/120px-WhatsApp.svg.png", "https://wa.me/5542991657847", "50px")
 
 
