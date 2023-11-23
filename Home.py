668d898fa17ce28e81cf5ff3d629697115b4d05e
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2023-11-23 20:56:38 -0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -80,8 +80,8 @@ st.markdown("")
 centrar_texto("Aplicacion para la administración de alquileres temporarios.", 5, "white")
 centrar_texto("Si desea una personalizacion de este proyecto consulte con nuestro desarrollador.", 5, "white")
 
-col8, col9, col10, col11, col12 = st.columns(5)
-with col10:
+col4, col5, col6 = st.columns([5, 3.5, 2])
+with col5:
     photo_link("Whatsapp", "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/120px-WhatsApp.svg.png", "https://wa.me/5542991657847", "50px")
 
 st.markdown("""<hr style="height:10px;border:none;color:#333;background-color:#333;" /> """, unsafe_allow_html=True)
@@ -91,8 +91,8 @@ st.markdown("")
 centrar_texto("Aplicativo para gerenciamento de aluguel temporário.", 5, "white")
 centrar_texto("Caso queira customização deste projeto consulte nosso desenvolvedor.", 5, "white")
 
-col21, col22, col23, col24, col25 = st.columns(5)
-with col23:
+col7, col8, col9 = st.columns([5, 3.5, 2])
+with col8:
     photo_link("Whatsapp", "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/120px-WhatsApp.svg.png", "https://wa.me/5542991657847", "50px")
 
 st.markdown("""<hr style="height:10px;border:none;color:#333;background-color:#333;" /> """, unsafe_allow_html=True)
