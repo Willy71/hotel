6d9210cf0ce2f68cbf23b84d07e8e9a52df9a246
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2023-11-23 16:25:56 -0300

    Update 4_Contact.py

--- pages/4_Contact.py
+++ pages/4_Contact.py
@@ -50,7 +50,7 @@ def centrar_texto(texto, tamanho, color):
                 unsafe_allow_html=True)
 
 def photo_link(nombre, imagen, link):
-    st.markdown(f"[![{nombre}]({imagen})]({link}){:width='80px'}", unsafe_allow_html=True)
+    st.markdown(f"[![{nombre}]({imagen})]({link}){:width='80px'}, unsafe_allow_html=True")
 
 st.write("#")
 
