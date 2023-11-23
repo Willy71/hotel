9517349205987bf37c4634dae2abed97af97b978
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2023-11-23 16:32:34 -0300

    Update 4_Contact.py

--- pages/4_Contact.py
+++ pages/4_Contact.py
@@ -50,7 +50,7 @@ def centrar_texto(texto, tamanho, color):
                 unsafe_allow_html=True)
 
 def photo_link(alt_text, img_url, link_url, img_width):
-    markdown_code = f"[![{alt_text}]({img_url})]({link_url}){{:width='{img_width}'}}"
+    markdown_code = f"[![{alt_text}]({img_url})]({link_url}){{:width=80px}}"
     st.markdown(markdown_code, unsafe_allow_html=True)
     
 st.write("#")
@@ -58,11 +58,11 @@ st.write("#")
 with st.container():    
     col50, col51, col52, col53, col54 = st.columns(5)
     with col51:
-        photo_link("Kaggle", "https://e7.pngegg.com/pngimages/399/47/png-clipart-kaggle-predictive-modelling-data-science-business-predictive-analytics-%E6%95%B0%E6%8D%AE-blue-text.png", "https://www.kaggle.com/willycerato", "80px")
+        photo_link("Kaggle", "https://e7.pngegg.com/pngimages/399/47/png-clipart-kaggle-predictive-modelling-data-science-business-predictive-analytics-%E6%95%B0%E6%8D%AE-blue-text.png", "https://www.kaggle.com/willycerato")
     with col52:
-        photo_link("Github", "https://static.vecteezy.com/system/resources/previews/016/833/880/non_2x/github-logo-git-hub-icon-with-text-on-white-background-free-vector.jpg", "https://github.com/Willy71", "80px")
+        photo_link("Github", "https://static.vecteezy.com/system/resources/previews/016/833/880/non_2x/github-logo-git-hub-icon-with-text-on-white-background-free-vector.jpg", "https://github.com/Willy71")
     with col53:
-        photo_link("Instagram", "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/150px-Instagram_logo_2022.svg.png", "https://www.instagram.com/willycerato", "80px")
+        photo_link("Instagram", "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/150px-Instagram_logo_2022.svg.png", "https://www.instagram.com/willycerato")
             
 st.title("")
 st.title("")
@@ -70,8 +70,8 @@ st.title("")
 with st.container():    
     col55, col56, col57, col58, col59 = st.columns(5)
     with col56:
-        photo_link("Facebook", "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/2023_Facebook_icon.svg/50px-2023_Facebook_icon.svg.png", "https://www.facebook.com/guillermo.cerato", "80px")
+        photo_link("Facebook", "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/2023_Facebook_icon.svg/50px-2023_Facebook_icon.svg.png", "https://www.facebook.com/guillermo.cerato")
     with col57:
-        photo_link("Linkedin", "https://img.freepik.com/vetores-premium/logotipo-quadrado-do-linkedin-isolado-no-fundo-branco_469489-892.jpg", "https://www.linkedin.com/in/willycerato", "80px")        
+        photo_link("Linkedin", "https://img.freepik.com/vetores-premium/logotipo-quadrado-do-linkedin-isolado-no-fundo-branco_469489-892.jpg", "https://www.linkedin.com/in/willycerato")        
     with col58:
-        photo_link("Whatsapp", "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/120px-WhatsApp.svg.png", "Whatsapp](https://wa.me/5542991657847", "80px")
+        photo_link("Whatsapp", "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/120px-WhatsApp.svg.png", "Whatsapp](https://wa.me/5542991657847")
