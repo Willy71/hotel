6fa48ccdceb69a10bdc27a9f0ba31ad0ee622cc1
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2023-11-24 19:31:39 -0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -4,6 +4,15 @@ import webbrowser
 from datetime import datetime
 from streamlit_gsheets import GSheetsConnection
 
+# Create a connection object.
+conn = st.connection("gsheets", type=GSheetsConnection)
+
+df = conn.read()
+
+# Print results.
+for row in df.itertuples():
+    st.write(f"{row.name} has a :{row.pet}:")
+
 # Colocar nome na pagina, icone e ampliar a tela
 st.set_page_config(
     page_title="Hotels Service",
