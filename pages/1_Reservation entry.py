fac99545c4e787a2168daa7e657ba8cc2d2f035b
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2023-11-24 09:27:05 -0300

    Update 1_Reservation entry.py

--- pages/1_Reservation entry.py
+++ pages/1_Reservation entry.py
@@ -161,10 +161,10 @@ with st.container():
 with st.container():    
     col81, col82, col83, col84 = st.columns(4)
     with col82:
-        opciones_pago = ["You will not pay a reservation", "Credit card", "Cash", "Debit"]
+        opciones_pago = ["None", "Credit card", "Cash", "Debit"]
         option = st.selectbox("Payment", opciones_pago, index=None, placeholder="Payment option...")
     with col83:
-        opciones_saldo = ["Full payment", "Partial payment", "No reservation payment"]
+        opciones_saldo = ["None", "Full payment", "Partial payment"]
         option = st.selectbox("Pay", opciones_saldo, index=None, placeholder="Pay...")
     with col84:
         pay = st.number_input('Insert a pay')
