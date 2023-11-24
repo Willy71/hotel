f3611eba15ed67ea97ff07803a84130dc946f862
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2023-11-24 19:29:51 -0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import webbrowser
 from datetime import datetime
-from streamlit_gsheets import GSheetsConnection
 
 # Colocar nome na pagina, icone e ampliar a tela
 st.set_page_config(
@@ -36,15 +35,6 @@ background: rgba(28,28,56,1);
 </style>
 """
 st.markdown(page_bg_img, unsafe_allow_html=True)
-
-# Create a connection object.
-conn = st.connection("gsheets", type=GSheetsConnection)
-
-df = conn.read()
-
-# Print results.
-for row in df.itertuples():
-    st.write(f"{row.name} has a :{row.pet}:")
 
 
 def centrar_imagen(imagen, ancho):
