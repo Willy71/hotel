3b6bb2d7741de37e0b997c6b2aee9d2a2ba6afb2
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2023-11-25 08:36:55 -0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -2,6 +2,18 @@ import streamlit as st
 import pandas as pd
 import webbrowser
 from datetime import datetime
+import os
+
+# Everything is accessible via the st.secrets dict:
+st.write("DB username:", st.secrets["db_username"])
+st.write("DB password:", st.secrets["db_password"])
+st.write("My cool secrets:", st.secrets["my_cool_secrets"]["things_i_like"])
+
+# And the root-level secrets are also accessible as environment variables:
+st.write(
+    "Has environment variables been set:",
+    os.environ["db_username"] == st.secrets["db_username"],
+)
 
 # Colocar nome na pagina, icone e ampliar a tela
 st.set_page_config(
