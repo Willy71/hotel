443df4b55710fc978d318ccc52f3fc8a75820042
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2023-12-03 16:00:04 -0300

    Update 1_Portal de gestão.py

--- pages/1_Portal de gestão.py
+++ pages/1_Portal de gestão.py
@@ -358,8 +358,8 @@ elif action == "Atualizar reserva existente":
                 room = st.selectbox("Quarto", opciones_numericas, placeholder="Selecione um quarto...", index=opciones_numericas.index(vendor_data["Quarto"]))
             with col311:
                 guests = st.selectbox("Quantidade de hospedes", opciones_numericas_2, placeholder="Hospedes...", index=opciones_numericas_2.index(vendor_data["Hospedes"]))
-
-        with st.container():    
+            
+        with st.container():
             col390, col391, col392, col393, col394 = st.columns([1, 1, 3, 1, 1])
             with col392:
                 update_button = st.form_submit_button(label="Atualizar reserva....")
