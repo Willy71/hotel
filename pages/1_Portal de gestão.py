70942ffb89b25744ae63683a125cdc82aabe3507
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2023-12-03 18:36:08 -0300

    Update 1_Portal de gestão.py

--- pages/1_Portal de gestão.py
+++ pages/1_Portal de gestão.py
@@ -452,6 +452,7 @@ elif action == "Atualizar reserva existente":
                     updated_vendor_data = pd.DataFrame(
                         [
                             {
+                                'user_id': obtener_proximo_id(existing_data),
                                 'Quarto': room,
                                 'Hospedes': guests,
                                 'Hora de entrada': checkin_time.isoformat() if checkin_time else None,
