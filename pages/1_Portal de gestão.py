0e0fffadf6d1b63618f8be5e58ddf759b461806e
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2023-12-04 13:45:03 -0300

    Update 1_Portal de gestão.py

--- pages/1_Portal de gestão.py
+++ pages/1_Portal de gestão.py
@@ -248,7 +248,7 @@ if action == "Adicionar nova reserva":
                 # Removing old entry
                 existing_data.drop(
                     existing_data[
-                        existing_data["User_id"] == vendor_to_update
+                        existing_data["user_id"] == vendor_to_update
                     ].index,
                     inplace=True,
                 )
