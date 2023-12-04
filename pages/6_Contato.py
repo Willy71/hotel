c695acc5922f1728d52f08aaea4017a7f44d4df5
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2023-12-03 21:42:28 -0300

    Update 6_Contato.py

--- pages/6_Contato.py
+++ pages/6_Contato.py
@@ -58,42 +58,42 @@ st.write("#")
 with st.container():    
     col00, col01, col02, col03, col04, col05 = st.columns(6)
     with col01:
-        photo_link("Facebook", "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/2023_Facebook_icon.svg/50px-2023_Facebook_icon.svg.png", "https://www.facebook.com/guillermo.cerato", "80px")
+        photo_link("Facebook", "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/2023_Facebook_icon.svg/50px-2023_Facebook_icon.svg.png", "https://www.facebook.com/guillermo.cerato", "50px")
     with col03:
         centrar_texto("Facebook", 4, "white")
         
 with st.container():    
     col10, col11, col12, col13, col14, col15 = st.columns(6)
     with col11:
-        photo_link("Instagram", "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/150px-Instagram_logo_2022.svg.png", "https://www.instagram.com/willycerato", "80px")
+        photo_link("Instagram", "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/150px-Instagram_logo_2022.svg.png", "https://www.instagram.com/willycerato", "50px")
     with col13:
         centrar_texto("Instagram", 4, "white")
 
 with st.container():    
     col20, col21, col22, col23, col24, col25 = st.columns(6)
     with col21:
-        photo_link("Linkedin", "https://img.freepik.com/vetores-premium/logotipo-quadrado-do-linkedin-isolado-no-fundo-branco_469489-892.jpg", "https://www.linkedin.com/in/willycerato", "80px")
+        photo_link("Linkedin", "https://img.freepik.com/vetores-premium/logotipo-quadrado-do-linkedin-isolado-no-fundo-branco_469489-892.jpg", "https://www.linkedin.com/in/willycerato", "50px")
     with col23:
         centrar_texto("Linkedin", 4, "white")
         
 with st.container():    
     col30, col31, col32, col33, col34, col35 = st.columns(6)
     with col31:
-        photo_link("Whatsapp", "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/120px-WhatsApp.svg.png", "https://wa.me/5542991657847", "80px")
+        photo_link("Whatsapp", "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/120px-WhatsApp.svg.png", "https://wa.me/5542991657847", "50px")
     with col33:
         centrar_texto("Whatsapp", 4, "white")
 
 with st.container():    
     col40, col41, col42, col43, col44, col45 = st.columns(6)
     with col41:
-        photo_link("Kaggle", "https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/189_Kaggle_logo_logos-512.png", "https://www.kaggle.com/willycerato", "80px")
+        photo_link("Kaggle", "https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/189_Kaggle_logo_logos-512.png", "https://www.kaggle.com/willycerato", "50px")
     with col43:
         centrar_texto("Kaggle", 4, "white")
 
 with st.container():    
     col50, col51, col52, col53, col54, col55 = st.columns(6)
     with col51:
-        photo_link("Github", "https://cdn.pixabay.com/photo/2022/01/30/13/33/github-6980894_1280.png", "https://github.com/Willy71", "80px")
+        photo_link("Github", "https://cdn.pixabay.com/photo/2022/01/30/13/33/github-6980894_1280.png", "https://github.com/Willy71", "50px")
     with col53:
         centrar_texto("GitHub", 4, "white")
 
