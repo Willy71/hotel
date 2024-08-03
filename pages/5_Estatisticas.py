205be1b2bac41f282beebc6d0e233bddbf9aa237
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2024-08-03 17:29:41 -0300

    Update 5_Estatisticas.py

--- pages/5_Estatisticas.py
+++ pages/5_Estatisticas.py
@@ -56,7 +56,8 @@ SPREADSHEET_KEY =  st.secrets["gsheets"]["SPREADSHEET_KEY"]  # Reemplaza con la 
 SHEET_NAME = 'Hoja1'  # Nombre de la hoja dentro del documento
 
 try:
-    existing_data = gc.open_by_key(SPREADSHEET_KEY).worksheet(SHEET_NAME)
+    worksheet = gc.open_by_key(SPREADSHEET_KEY).worksheet(SHEET_NAME)
+    existing_data = pd.DataFrame(worksheet.get_all_records())
 except gspread.exceptions.SpreadsheetNotFound:
     st.error(f"No se encontró la hoja de cálculo con la clave '{SPREADSHEET_KEY}'. Asegúrate de que la clave es correcta y que has compartido la hoja con el correo electrónico del cliente de servicio.")
 #=============================================================================================================================
